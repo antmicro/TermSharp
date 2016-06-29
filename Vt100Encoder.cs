e65414ecc7f7912da3711187c6a2d6e321ec7807
Konrad M. Kruczyński <kkruczynski@antmicro.com>
2016-06-29 17:13:22 +0200

    Separate collection for directly passed keys.

--- Vt100Encoder.cs
+++ Vt100Encoder.cs
@@ -14,9 +14,13 @@ namespace Terminal
         public Vt100Encoder(Action<byte> dataCallback)
         {
             this.dataCallback = dataCallback;
+            directlyPassedKeys = new HashSet<Key>
+            {
+                Key.K1, Key.K2, Key.K3, Key.K4, Key.K5, Key.K6, Key.K7, Key.K8, Key.K9, Key.K0,
+                Key.Space
+            };
             keyHandlers = new Dictionary<Key, byte[]>
             {
-                { Key.Space, new [] { (byte)0x20 } }, // TODO
                 { Key.Return, new [] { (byte)ControlByte.CarriageReturn } },
                 { Key.BackSpace, new [] { (byte)ControlByte.Backspace } },
                 { Key.Tab, new [] { (byte)ControlByte.HorizontalTab } },
@@ -29,7 +33,7 @@ namespace Terminal
 
         public void Feed(Key key, ModifierKeys modifiers)
         {
-            if((key >= Key.A && key <= Key.Z) || (key >= Key.a && key <= Key.z))
+            if((key >= Key.A && key <= Key.Z) || (key >= Key.a && key <= Key.z) || directlyPassedKeys.Contains(key))
             {
                 dataCallback((byte)key);
                 return;
@@ -44,6 +48,7 @@ namespace Terminal
             }
         }
 
+        private readonly HashSet<Key> directlyPassedKeys;
         private readonly Action<byte> dataCallback;
         private readonly Dictionary<Key, byte[]> keyHandlers;
     }
